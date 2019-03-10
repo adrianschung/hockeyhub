100fad2488f06e79dc223d224e5feb37ff3e95fa
adrianschung <adrianschung@gmail.com>
2019-03-10 19:18:13 -0400

    Add error routes

---
+++ app/api/nhl_api.rb
@@ -0,0 +1,23 @@
+module NHLAPI
+  class Team
+
+    def initialize(id)
+      @id = id
+      @parsed_response = nil
+    end
+
+    def get_stats
+      url = "https://statsapi.web.nhl.com/api/v1/teams/#{id}/stats" 
+      response = HTTParty.get(url)
+      @parsed_response = JSON.parse(response.body, symbolize_names: true)
+      @parsed_response[:stats][0][:splits][0][:stat]
+    end
+
+    def get_roster
+      url = "https://statsapi.web.nhl.com/api/v1/teams/#{id}/roster" 
+      response = HTTParty.get(url)
+      @parsed_response = JSON.parse(response.body, symbolize_names: true)
+      @parsed_response[:roster][0]
+    end
+  end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
   resources :teams, :path => '', param: :slug, only: [:show]
+  %w( 404 422 500 503 ).each do |code|
+    get code, :to => "errors#show" :code => code
+  end
 end
