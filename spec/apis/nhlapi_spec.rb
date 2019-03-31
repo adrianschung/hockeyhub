d522ffba3b1501b5d608d0b1a29829dff1514dc6
adrianschung <adrianschung@gmail.com>
2019-03-31 16:03:26 -0400

    change test to check for valid stats

--- spec/apis/nhlapi_spec.rb
+++ spec/apis/nhlapi_spec.rb
@@ -1,12 +1,15 @@
 require 'rails_helper'
 
-describe NHLAPI do 
-   context 'When getting stats of a team' do 
-      
-      it 'retrieves a team when id is valid' do 
-         request = NHLAPI::Team.new(1).get_stats
-         expect(request[:wins]).to be_an(Integer)
-      end
-      
-   end
+describe NHLAPI do
+  context 'When getting stats of a team' do
+
+    it 'retrieves stats of a valid team' do
+      request = NHLAPI::Team.new(1).get_stats
+      expect(request[:wins]).to be_an(Integer)
+      expect(request[:gamesPlayed]).to be_an(Integer)
+      expect(request[:losses]).to be_an(Integer)
+      expect(request[:ot]).to be_an(Integer)
+      expect(request[:pts]).to be_an(Integer)
+    end
+  end
 end
