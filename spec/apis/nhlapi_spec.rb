61c35aa3d21f60b3ec5027690f541dc8d551bcbf
adrianschung <adrianschung@gmail.com>
2019-03-31 16:04:33 -0400

    add basic test for stat retrieval

--- spec/apis/nhlapi_spec.rb
+++ spec/apis/nhlapi_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 describe NHLAPI do
   context 'When getting stats of a team' do
 
+    it 'retrieves a valid team' do
+      request = NHLAPI::Team.new(1).get_stats
+      expect(request).not_to be_empty
+    end
+
     it 'retrieves stats of a valid team' do
       request = NHLAPI::Team.new(1).get_stats
       expect(request[:wins]).to be_an(Integer)
