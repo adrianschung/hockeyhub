9f5e6baad4936400bf8bda533ed496c5e8049175
adrianschung <adrianschung@gmail.com>
2019-03-31 16:09:50 -0400

    add basic test for stat retrieval

--- app/apis/nhlapi.rb
+++ app/apis/nhlapi.rb
@@ -10,13 +10,13 @@ module NHLAPI
     end
 
     def get_stats
-      url = "https://statsapi.web.nhl.com/api/v1/teams/#{@id}/stats" 
+      url = "https://statsapi.web.nhl.com/api/v1/teams/#{@id}/stats"
       @parsed_response = get_response(url)
       @parsed_response[:stats][0][:splits][0][:stat]
     end
 
     def get_roster
-      url = "https://statsapi.web.nhl.com/api/v1/teams/#{@id}/roster" 
+      url = "https://statsapi.web.nhl.com/api/v1/teams/#{@id}/roster"
       @parsed_response = get_response(url)
       @parsed_response[:roster]
     end
